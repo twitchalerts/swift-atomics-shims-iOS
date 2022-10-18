545693c65305ecd70ac6ad147b731b6e716dd973
leo <sokolov.lev95@gmail.com>
2022-10-18 14:14:17 -0700

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2020-2021 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import PackageDescription
+
+let package = Package(
+  name: "swift-atomics-shims",
+  products: [
+    .library(
+      name: "_AtomicsShims",
+      targets: ["_AtomicsShims"]),
+  ],
+  targets: [
+    .target(
+      name: "_AtomicsShims"
+    ),
+  ]
+)
